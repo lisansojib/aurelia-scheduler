dacfcced2f58e355edeb08d0ac69e2741eae0f50
Sojib <Sojib>
2016-11-08 20:57:31 +0600

    Initial Commit for Aurelia Scheduler Project.

---
+++ Scheduler.Api/Startup.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Scheduler.Data;
+using Scheduler.Data.Repositories;
+using Scheduler.Data.Abstract;
+using System.Net;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Diagnostics;
+using Scheduler.Api.Core;
+using Scheduler.Api.ViewModels.Mappings;
+using Scheduler.Data.Scheduler.Data;
+
+namespace Scheduler.Api
+{
+    public class Startup
+    {
+        private static string _applicationPath = string.Empty;
+        private static string _contentRootPath = string.Empty;
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            _applicationPath = env.WebRootPath;
+            _contentRootPath = env.ContentRootPath;
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_contentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string sqlConnectionString = Configuration["Data:SchedulerConnection:ConnectionString"];
+            bool useInMemoryProvider = bool.Parse(Configuration["Data:SchedulerConnection:InMemoryProvider"]);
+
+            services.AddDbContext<SchedulerContext>(options => {
+                switch (useInMemoryProvider)
+                {
+                    case true:
+                        options.UseInMemoryDatabase();
+                        break;
+                    default:
+                        options.UseSqlServer(Configuration["Data:SchedulerConnection:ConnectionString"],
+                    b => b.MigrationsAssembly("Scheduler.Api"));
+                        break;
+                }
+            });
+
+
+            // Repositories
+            services.AddScoped<IScheduleRepository, ScheduleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+
+            // Automapper Configuration
+            AutoMapperConfiguration.Configure();
+
+            // Enable Cors
+            services.AddCors();
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+                .AddJsonOptions(opts =>
+                {
+                    // Force Camel Case to JSON
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            // Add MVC to the request pipeline.
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseExceptionHandler(
+              builder =>
+              {
+                  builder.Run(
+                    async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+                        }
+                    });
+              });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            SchedulerDbInitializer.Initialize(app.ApplicationServices);
+        }
+    }
+}
---
+++ Scheduler.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scheduler.Model")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("244cfba5-6a64-4ddf-945f-951e2560bb9f")]
